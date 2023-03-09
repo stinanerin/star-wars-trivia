da84020d0938ff2720b65998cf2488dafe8f4611
stormstina <stina.nerin@gmail.com>
2023-03-09 10:58:38 +0100

    Add resuable function for creating new instance

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ console.log("Star Wars Trivia");
 
 let charContainer = document.querySelector("#compareCharacter")
 let charForm = document.querySelector("#characterForm")
+let compareBtn = document.querySelector("#compareBtn")
+
 let charArr = []
+let errorData = false;
 
 // -------------------------------------------------------- Set up: API -----------------------------------------------------------
 
@@ -30,23 +33,26 @@ class Character {
         this.pictureUrl = pictureUrl + ".svg";
     }
     renderCharacter() {
+        console.log("hej");
         charContainer.innerHTML += `
             <article class="col" data-character="${this.name.toLowerCase().split(' ').join("-")}">
-                <h3>${(this.name).toLowerCase()}</h3>
-                <img src="/images/${this.pictureUrl.toLowerCase().split(' ').join("-")}" alt="Portrait of ${this.name}"/>
+                <div class="text-center">
+                    <h3>${(this.name).toLowerCase()}</h3>
+                    <img src="/images/${this.pictureUrl.toLowerCase().split(' ').join("-")}" alt="Portrait of ${this.name}"/>
+                </div>
             </article>
         `
     }
     renderProperties(container) {
         container.innerHTML += `
             <article class="col">
-                <ul> 
-                    <li><span>Hair color: </span>${(this.hairColor)}</li>
-                    <li><span>Gender: </span>${(this.gender)}</li>
-                    <li><span>Heigth: </span>${(this.height)}</li>
-                    <li><span>Mass: </span>${(this.mass)}</li>
-                    <li><span>Skin color: </span>${(this.skinColor)}</li>
-                    <li><span>Movies: </span>${(this.movies.length)}</li>
+                <ul class="list-group"> 
+                    <li class="list-group-item"><span>Hair color: </span>${(this.hairColor)}</li>
+                    <li class="list-group-item"><span>Gender: </span>${(this.gender)}</li>
+                    <li class="list-group-item"><span>Heigth: </span>${(this.height)}</li>
+                    <li class="list-group-item"><span>Mass: </span>${(this.mass)}</li>
+                    <li class="list-group-item"><span>Skin color: </span>${(this.skinColor)}</li>
+                    <li class="list-group-item"><span>Movies: </span>${(this.movies.length)}</li>
                 </ul>
             </article>
         `
@@ -56,78 +62,34 @@ class Character {
 // -------------------------------------------------------- Choose Character - Form Event Listener -----------------------------------------------------------
 
 charForm.addEventListener("submit", (e) => {
-    e.preventDefault()
 
-    charContainer.innerHTML = "";
-    let charOne = document.querySelector("#charOne").value
-    let charTwo = document.querySelector("#charTwo").value
+    e.preventDefault()
+    let charOneInput = document.querySelector("#charOne").value
+    let charTwoInput = document.querySelector("#charTwo").value
+    compareBtn.classList.remove("hidden")
     charForm.classList.add("hidden")
 
-    let compareButton = document.createElement("button")
-    compareButton.innerText = "compare characters"
-    compareButton.addEventListener('click', compareCharacter);
+    //todo! Undersök varför loop ej funkar - se graveyard
+    loadCharacters(charOneInput).then(() => {
 
-    route = "people/?"
-
-    let paramsCharOne = new URLSearchParams({
-        //todo! ta bort !== "any"
-        ...(charOne !== "any" ? { search: charOne } : {})
+        loadCharacters(charTwoInput).then(() => {
+            if(errorData) {
+                console.log("errorData i 2:a", errorData);
+                compareBtn.classList.add("hidden")
+                charContainer.innerHTML= ""
+                let h3 = document.createElement("h3");
+                h3.innerText = "Sorry, we had problems getting the data.. Try again later.";
+                document.body.append(h3);
+            }
+        })
     })
-
-    let paramsCharTwo = new URLSearchParams({
-        //todo! ta bort !== "any"
-        ...(charTwo !== "any" ? { search: charTwo } : {})
-    })
-
-    console.log(`${API_BASE_URL}${route}${paramsCharOne}`);
-    // console.log(`${API_BASE_URL}${route}${paramsCharTwo}`);
-
-    getData(route, paramsCharOne)
-    .then((obj) => {
-
-        let { name, gender, height, mass, hair_color, skin_color, eye_color, films } = obj.results[0];
-        
-        let charOneProto = new Character(name, gender, height, mass, hair_color, skin_color, eye_color, films, name)
-        charArr.push(charOneProto)
-
-        console.log(charOneProto);
-
-        charOneProto.renderCharacter()
-    
-        //todo! error hantering - om karaktären ej finns
-        //todo! error hantering - om anv. ej valt två karaktärer
-
-    })
-    .catch((error) =>  {
-        console.log(error);
-        let h2 = document.createElement("h2");
-        h2.innerText = "Sorry, we had problems getting the data.. Try again later.";
-        document.body.append(h2);
-    })
-    getData(route, paramsCharTwo)
-    .then((obj) => {
-
-        // Destructuring
-        let { name, gender, height, mass, hair_color, skin_color, eye_color, films } = obj.results[0];
-
-        let charTwoProto = new Character(name, gender, height, mass, hair_color, skin_color, eye_color, films, name)
-        charArr.push(charTwoProto)
-
-        console.log(charTwoProto);
-
-        charTwoProto.renderCharacter()
-    
-        //todo! error hantering - om karaktären ej finns
-        //todo! error hantering - om anv. ej valt två karaktärer
-
-        charContainer.append(compareButton)
-
-    })
+    console.log("outside",charArr);
 })
 
 // -------------------------------------------------------- Compare Character -----------------------------------------------------------
 
 let compareCharacter = () => {
+    console.log("clicked");
     console.log(charArr);
     event.target.classList.add("hidden")
 
@@ -138,5 +100,38 @@ let compareCharacter = () => {
         obj.renderProperties(article)
 
     })
+}
+// -------------------------------------------------------- Load characters -----------------------------------------------------------
+
+
+let loadCharacters = async (charInput) => {
+    //todo! error hantering - om karaktären ej finns
+    //todo! error hantering - om anv. ej valt två karaktärer - 
+    //todo! dubbelkolla båda om första och andra ej existerar
+    try {
+        route = "people/?"
+
+        let params = new URLSearchParams({
+            //todo! ta bort !== "any"
+            ...(charInput !== "any" ? { search: charInput } : {})
+        })
+    
+        console.log(`${API_BASE_URL}${route}${params}`);
+
+        let charObj = await getData(route, params)
+
+        let { name, gender, height, mass, hair_color, skin_color, eye_color, films } = charObj.results[0];
+        
+        //todo! fixa dynamiskt namn?
+        let charOneProto = new Character(name, gender, height, mass, hair_color, skin_color, eye_color, films, name)
+        charArr.push(charOneProto)
+
+        console.log(charOneProto);
+        charOneProto.renderCharacter()
+           
+    } catch (error) {
+        errorData = true
+        console.log(error);
+    }
 
 }
